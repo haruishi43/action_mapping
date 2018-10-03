a22c13e20fa749fe3e3cdfd34a81dec43a8eaa37
Toraudonn <toraudonn@gmail.com>
2018-10-03 23:45:56 +0900

    tried socket

--- viewer/streamer.py
+++ viewer/streamer.py
@@ -1,35 +1,42 @@
+import os
+import sys
 import cv2
 import threading
 import socket
 import struct
-import StringIO
+import io
 import json
-import numpy
+import numpy as np
 
-class Streamer (threading.Thread):
+
+
+class Streamer(threading.Thread):
+
+
     def __init__(self, hostname, port):
         threading.Thread.__init__(self)
 
-    self.hostname = hostname
-    self.port = port
-    self.connected = False
-    self.jpeg = None
+        self.hostname = hostname
+        self.port = port
+        self.connected = False
+        self.jpeg = None
+
 
     def run(self):
 
         self.isRunning = True
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print 'Socket created'
+        print('Socket created')
 
         s.bind((self.hostname, self.port))
-        print 'Socket bind complete'
+        print('Socket bind complete')
 
         data = ""
         payload_size = struct.calcsize("L")
 
         s.listen(10)
-        print 'Socket now listening'
+        print('Socket now listening')
 
         while self.isRunning:
 
@@ -37,32 +44,21 @@ class Streamer (threading.Thread):
 
             while True:
 
-            data = conn.recv(4096)
+                data = conn.recv(4096).decode('utf-8')
 
-            if data:
-                packed_msg_size = data[:payload_size]
-                data = data[payload_size:]
-                msg_size = struct.unpack("L", packed_msg_size)[0]
+                if data:
+                    print(data)
+                    frame = np.asarray(json.loads(data))
 
-                while len(data) < msg_size:
-                data += conn.recv(10000)
+                    ret, jpeg = cv2.imencode('.jpg', frame)
+                    self.jpeg = jpeg
 
-                frame_data = data[:msg_size]
+                    self.connected = True
 
-                memfile = StringIO.StringIO()
-                memfile.write(json.loads(frame_data).encode('latin-1'))
-                memfile.seek(0)
-                frame = numpy.load(memfile)
-
-                ret, jpeg = cv2.imencode('.jpg', frame)
-                self.jpeg = jpeg
-
-                self.connected = True
-
-            else:
-                conn.close()
-                self.connected = False
-                break
+                else:
+                    conn.close()
+                    self.connected = False
+                    break
 
         self.connected = False
 
